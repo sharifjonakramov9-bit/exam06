53705a27af043007d867fae1306773583720bbcf
sharifjonakramov9.bit <sharifjonakramov9@gemail.com>
2026-01-29 20:24:50 +0300

    number_1

---
+++ students/urls.py
@@ -0,0 +1,8 @@
+# 1 Studentlar ro'yxati
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.student_list, name='student_list'), 
+]
--- students/views.py
+++ students/views.py
@@ -1,3 +1,24 @@
+# 1 Studentlar ro'yxati
 from django.shortcuts import render
+from .models import Student
+from django.db.models import Q
 
-# Create your views here.
+
+def student_list(request):
+    students = Student.objects.all()
+
+    min_age = request.GET.get('search')
+    if min_age:
+        students = students.filter(age__gte=min_age)
+
+    search = request.GET.get(search)
+    if search:
+        students = students.filter(
+            Q(full_name__icontains=search) |
+            Q(email__icontains=search)
+        )
+
+    context = {
+        'students': students
+        }
+    return render(request, 'students/student_list.html', context)
