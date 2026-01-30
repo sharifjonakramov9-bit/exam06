bc88c42fce9e15fc3961eee3349caf0ee68a8d9b
sharifjonakramov9.bit <sharifjonakramov9@gemail.com>
2026-01-30 06:31:46 +0300

    fixed

--- core/urls.py
+++ core/urls.py
@@ -18,8 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
     path('courses/', include('courses.urls')),
     path('stundents/', include('students.urls')),
-    path('enrollments/', include('enrollment.urls')),
+    path('enrollments/', include('enrollments.urls')),
 ]
