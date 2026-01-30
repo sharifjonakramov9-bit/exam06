fa18c1d4264a075266c12b821eb64ace07756dda
sharifjonakramov9.bit <sharifjonakramov9@gemail.com>
2026-01-30 06:43:19 +0300

    done

--- core/settings.py
+++ core/settings.py
@@ -1,3 +1,9 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
 """
 Django settings for core project.
 
@@ -20,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r^&sw^ck+##u2e9qve5w%oh*hqbt6+3nbvrd!@rl%quet!*(*e'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
